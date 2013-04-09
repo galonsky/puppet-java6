732250888706ee809f5fb6e0cf2a95d5fad5aaff
Carter McKendry <carter.mckendry@gmail.com>
2013-04-09 17:36:04 -0300

    Changing package provider type
    Hopefully it's that simple...

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #  include java6
 class java6 {
   package { 'Java6':
-    provider => 'appdmg',
+    provider => 'pkgdmg',
     source   => 'http://supportdownload.apple.com/download.info.apple.com/Apple_Support_Area/Apple_Software_Updates/Mac_OS_X/downloads/041-9846.20130303.CPp69/JavaForOSX2013-002.dmg',
   }
 }
