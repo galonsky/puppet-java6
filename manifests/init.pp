3dcb7dcee3373bbbc87e95fc6f1b7c9cc5943d14
Jessica Gonzalez <jessica@loyal3.com>
2013-11-22 15:34:36 -0800

    Upgraded java to the 2013-05 release (1.6.0_65).

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class java6 {
   package { 'Java6':
     provider => 'pkgdmg',
-    source   => 'http://supportdownload.apple.com/download.info.apple.com/Apple_Support_Area/Apple_Software_Updates/Mac_OS_X/downloads/041-9846.20130303.CPp69/JavaForOSX2013-002.dmg',
+    source   => 'http://supportdownload.apple.com/download.info.apple.com/Apple_Support_Area/Apple_Software_Updates/Mac_OS_X/downloads/091-7364.20131014.Mmg65/JavaForOSX2013-05.dmg',
   }
 }
