86a0e70c64286762d11a2e0b31320e38d2b0e7a2
Robert Burns <burnsra@gmail.com>
2014-10-08 07:53:42 -0400

    Update to java6 version 2014-001

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class java6 {
   package { 'Java6':
     provider => 'pkgdmg',
-    source   => 'http://supportdownload.apple.com/download.info.apple.com/Apple_Support_Area/Apple_Software_Updates/Mac_OS_X/downloads/091-7364.20131014.Mmg65/JavaForOSX2013-05.dmg',
+    source   => 'https://s3.amazonaws.com/burnsra/JavaForOSX2014-001.dmg',
   }
 }
